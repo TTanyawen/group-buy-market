7598c5a92c9f11b24ac60cc3e3142133d2961c11
Tan Yawen <2749934612@qq.com>
2025-02-10 17:27:11 +0800

    feat: 2-2 framework 定义模板，规则树

---
+++ group-buy-market-domain/src/main/java/org/example/domain/activity/service/trial/factory/DefaultActivityStrategyFactory.java
@@ -0,0 +1,39 @@
+package org.example.domain.activity.service.trial.factory;
+
+import org.example.domain.activity.model.entity.MarketProductEntity;
+import org.example.domain.activity.model.entity.TrialBalanceEntity;
+import org.example.domain.activity.service.trial.node.RootNode;
+import org.example.types.design.framework.tree.StrategyHandler;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 活动策略工厂
+ * @create 2024-12-14 13:41
+ */
+@Service
+public class DefaultActivityStrategyFactory {
+
+    private final RootNode rootNode;
+
+    public DefaultActivityStrategyFactory(RootNode rootNode) {
+        this.rootNode = rootNode;
+    }
+
+    public StrategyHandler<MarketProductEntity, DynamicContext, TrialBalanceEntity> strategyHandler() {
+        return rootNode;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class DynamicContext {
+
+    }
+
+}
